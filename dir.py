d77a006031ce32a5beb18018e0f5b84095421980
Samyak Srivastava <samyaksrivastava9@gmail.com>
2021-12-14 02:40:04 +0530

    Add files via upload

---
+++ dir.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
+import string
+import nltk
+import warnings
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+warnings.filterwarnings('ignore')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
